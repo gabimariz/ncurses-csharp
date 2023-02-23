c00384f2a42926391f9a623c715f21a68a616e37
Gabriel Mariz <gabriellucena997@outlook.com>
2023-02-23 14:55:52 -0300

    :sparkles: ncurses attributes

---
+++ NCursesCSharp/Enums/NCursesAttrT.cs
@@ -0,0 +1,21 @@
+namespace NCursesCSharp.Enums;
+
+public enum NCursesAttrT : uint
+{
+	ANormal = 0U,
+	AStandOut = 65536U,
+	AUnderline = 131072U,
+	AReverse = 262144U,
+	ABlink = 524288U,
+	ADim = 1048576U,
+	ABold = 2097152U,
+	AAltcharset = 4194304U,
+	AInvis = 8388608U,
+	AProtect = 16777216U,
+	AHorizontal = 33554432U,
+	ALeft = 67108864U,
+	ALow = 134217728U,
+	ARight = 268435456U,
+	ATop = 536870912U,
+	AVertical = 1073741824U
+}
